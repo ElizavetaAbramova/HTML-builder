10ff737871f86cbc82adee4b324cf6219f054104
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-01-15 18:40:08 +0400

    fix: correct function

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = 'D:/projects/HTML-builder/01-read-file/text.txt';
+const path = __dirname + '\\text.txt';
 const readStream = fs.createReadStream(path);
 readStream.on('data', (text) => console.log(text.toString()));
 
