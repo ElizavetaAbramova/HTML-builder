f6cc8459e1812fce3c39f69a875b0ffc67a2df92
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-01-20 14:31:53 +0400

    fix: correct function that fill index.html file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const fsPromise = require('fs').promises;
 const path = require('path');
-// const replace = require('replace');
 const projectFolder = path.join(__dirname, 'project-dist');
 const folderWithStyles = path.join(__dirname, 'styles');
 const assetsFolder = path.join(__dirname, 'assets');
@@ -13,52 +12,77 @@ const templateFile = path.join(__dirname, 'template.html');
 const componentsFolder = path.join(__dirname, 'components');
 
 //create folder for project
-fs.mkdir(projectFolder, { recursive: true }, (err) => {
-  if (err) console.error(err);
-});
-//copy all folder assets to project folder
-fs.cp(
-  assetsFolder,
-  path.join(projectFolder, 'assets'),
-  { recursive: true },
-  (err) => {
-    if (err) console.log(err);
-  },
-);
-//copy all styles to style.css
-fs.readdir(folderWithStyles, (err, files) => {
-  if (err) console.log(err);
-  files.forEach((file) => {
-    const pathToFile = path.join(folderWithStyles, file);
-    if (path.extname(file) === '.css') {
-      fs.stat(path.join(folderWithStyles, file), (err, stats) => {
-        if (err) console.log(err);
-        if (stats.isFile()) {
-          const styles = fs.createReadStream(pathToFile, 'utf-8');
-          styles.on('data', (dataChunk) => mergedStyles.write(dataChunk));
-        }
-      });
-    }
+function createProjectFolder() {
+  fs.mkdir(projectFolder, { recursive: true }, (err) => {
+    if (err) console.error(err);
   });
-});
-
-//create index.html filling
-async function fillIndexFile() {
-  fs.copyFile(templateFile, indexFile, (err) => err);
-  let contentIndexFile = await fsPromise.readFile(indexFile, 'utf-8', (err) => err);
-  let filesInComponentsFolder = await fsPromise.readdir(componentsFolder, { withFileTypes: true });
-
-  filesInComponentsFolder.forEach(async (file) => {
-    const pathToFile = path.join(componentsFolder, file.name);
-    const fileExtension = path.extname(pathToFile);
-
-    if (path.extname(pathToFile) === '.html') {
-      const fileContent = await fsPromise.readFile(pathToFile, 'utf-8', (err) => err);
-      const fileName = file.name.replace(fileExtension, '');
-      contentIndexFile = contentIndexFile.replace(`{{${fileName}}}`, fileContent);
-      await fsPromise.writeFile(indexFile, contentIndexFile);
-    }
+}
+//copy all folder assets to project folder
+function copyAssets() {
+  fs.cp(
+    assetsFolder,
+    path.join(projectFolder, 'assets'),
+    { recursive: true },
+    (err) => {
+      if (err) console.log(err);
+    },
+  );
+}
+//copy all styles to style.css
+function copyStyles() {
+  fs.readdir(folderWithStyles, (err, files) => {
+    if (err) console.log(err);
+    files.forEach((file) => {
+      const pathToFile = path.join(folderWithStyles, file);
+      if (path.extname(file) === '.css') {
+        fs.stat(path.join(folderWithStyles, file), (err, stats) => {
+          if (err) console.log(err);
+          if (stats.isFile()) {
+            const styles = fs.createReadStream(pathToFile, 'utf-8');
+            styles.on('data', (dataChunk) => mergedStyles.write(dataChunk));
+          }
+        });
+      }
+    });
   });
 }
 
-fillIndexFile();
+function readFileWithFS(file) {
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) console.log(err);
+    readDirectory(componentsFolder, data);
+  });
+}
+
+function readDirectory(folder, data) {
+  fs.readdir(folder, {withFileTypes: true}, (err, files) => {
+    if (err) console.log(err);
+    files.forEach(async (file) => {
+      const pathToFile = path.join(componentsFolder, file.name);
+
+      if (path.extname(pathToFile) === '.html') {
+        const fileContent = await fsPromise.readFile(pathToFile, 'utf-8', (err) => err);
+        const fileName = file.name.slice(0, file.name.indexOf('.'));
+        data = data.replaceAll(`{{${fileName}}}`, fileContent);
+        await fsPromise.writeFile(indexFile, data);
+      }
+    });
+  });
+}
+
+//create index.html filling
+function fillIndexFile() {
+  fs.copyFile(templateFile, indexFile, (err) => {
+    if (err) console.log(err);
+    readFileWithFS(indexFile);
+  });
+}
+
+async function bundle() {
+  await createProjectFolder();
+  await copyAssets();
+  await copyStyles();
+  fillIndexFile();
+}
+
+bundle();
