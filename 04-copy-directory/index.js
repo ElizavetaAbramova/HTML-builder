8bbae6cbe0a10f5c4bd35230d5bf96c5d3fead2e
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-01-20 15:00:39 +0400

    refactor

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,24 +4,18 @@ const folderToCopy = 'files';
 const folderWithCopies = 'files-copy';
 
 fs.mkdir(path.join(__dirname, folderWithCopies), { recursive: true }, (err) => {
-  if (err) {
-    console.error(err);
-  }
+  if (err) console.error(err);
 });
 
 fs.readdir(path.join(__dirname, folderToCopy), (err, files) => {
-  if (err) {
-    console.log(err);
-  }
+  if (err) console.log(err);
   files.forEach((item) => {
     fs.copyFile(
       path.join(__dirname, folderToCopy, item),
       path.join(__dirname, folderWithCopies, item),
       (err) => {
-        if (err) {
-          console.error(err);
-        }
-        console.log(`Файл ${item} успешно копирован`);
+        if (err) console.error(err);
+        console.log(`File ${item} successfully copied`);
       },
     );
   });
