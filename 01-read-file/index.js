b4e571f1b6bd0aa1e721980cd366c3ec908b81ce
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-01-23 10:14:54 +0400

    fix: correct path to file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const path = __dirname + '\\text.txt';
-const readStream = fs.createReadStream(path);
+const path = require('path');
+const fileName = 'text.txt';
+const pathToFile = path.join(__dirname, fileName);
+const readStream = fs.createReadStream(pathToFile);
 readStream.on('data', (text) => console.log(text.toString()));
 
 // method without stream
